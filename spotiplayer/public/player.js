362ef3e7bc825f4046c27c47cbfba976143e3a81
Glitch (spotiplayer) <none>
2021-11-30 19:21:40 +0000

    🏰🏖 Checkpoint ./spotiplayer/public/player.html:15402040/13 ./spotiplayer/public/style.css:15402040/323 ./spotiplayer/public/player.js:15402040/4913

--- spotiplayer/public/player.js
+++ spotiplayer/public/player.js
@@ -51,6 +51,16 @@ PLAYER
 ------------------------------------------------
 */
 
+fetch("https://api.spotify.com/v1/me", {headers: {Authorization: getParameterByName()}})
+          .then(res => res.json())
+          .then(data => {
+            if (data.product == "premium") {
+              document.querySelector(".note").style.display = "none"
+            } else {
+              document.querySelector(".note").style.display = "block"
+            }
+        })
+
 setInterval(function() {
   fetch("https://api.spotify.com/v1/me/player/currently-playing?market=GB", {
     method: "get",
@@ -62,15 +72,6 @@ setInterval(function() {
     .then(response => response.json())
     .then(data => {
       try {
-        fetch("https://api.spotify.com/v1/me", {headers: {Authorization: getParameterByName()}})
-          .then(res => res.json())
-          .then(data => {
-            if (data.product == "premium") {
-              document.querySelector(".note").style.display = "none"
-            } else {
-              document.querySelector(".note").style.display = "block"
-            }
-        })
         fetch("https://api.spotify.com/v1/me/player", {
           method: "get",
           headers: {
